dce898dc4bfe06b71e85c9b642f445e59fb38092
DryingCore <lein0rb4g@gmail.com>
2024-06-30 02:52:45 +0000

    Jun 29, 2024, 11:52 PM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import ToDoComponent from "./components/ToDoComponent";
 import { addToDo, deleteToDo, getAllToDo, updateToDo } from "./utils/HandlerApi";
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Card, Typography } from "@mui/material";
 
 function App() {
 	const [toDo, setToDo] = useState([]);
@@ -22,20 +22,20 @@ function App() {
 	return (
 		<Box>
 			<Container>
-				<Grid container>
-					<div className="top">
-						<input type="text" name="" id="" placeholder="Add Task..." value={text} onChange={ev => setText(ev.target.value)} />
-						<div className="add" onClick={isUpdating ? () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating) : () => addToDo(text, setText, setToDo)}>
-							{isUpdating ? "Update" : "Add"}
-						</div>
+				<div className="top">
+					<input type="text" name="" id="" placeholder="Add Task..." value={text} onChange={ev => setText(ev.target.value)} />
+					<div className="add" onClick={isUpdating ? () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating) : () => addToDo(text, setText, setToDo)}>
+						{isUpdating ? "Update" : "Add"}
 					</div>
+				</div>
 
-					<div>
+				<Card sx={{width: "50%", backgroundColor: "gray", padding: "20px"}}>
+					<Typography>
 						{toDo.map(item => (
 							<ToDoComponent key={item._id} text={item.text} updateMode={() => updateMode(item._id, item.text)} deleteToDo={() => deleteToDo(item._id, setToDo)} />
 						))}
-					</div>
-				</Grid>
+					</Typography>
+				</Card>
 			</Container>
 		</Box>
 	);
