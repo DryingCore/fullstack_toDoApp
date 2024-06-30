34e5f4dfc3ec533e788da9b98e53686237d5b4e8
DryingCore <lein0rb4g@gmail.com>
2024-06-30 02:50:31 +0000

    Jun 29, 2024, 11:50 PM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import ToDoComponent from "./components/ToDoComponent";
 import { addToDo, deleteToDo, getAllToDo, updateToDo } from "./utils/HandlerApi";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 
 function App() {
 	const [toDo, setToDo] = useState([]);
@@ -21,21 +21,21 @@ function App() {
 
 	return (
 		<Box>
-			<Container className="container">
-				<h1>To Do App</h1>
-
-				<div className="top">
-					<input type="text" name="" id="" placeholder="Add Task..." value={text} onChange={ev => setText(ev.target.value)} />
-					<div className="add" onClick={isUpdating ? () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating) : () => addToDo(text, setText, setToDo)}>
-						{isUpdating ? "Update" : "Add"}
+			<Container>
+				<Grid container>
+					<div className="top">
+						<input type="text" name="" id="" placeholder="Add Task..." value={text} onChange={ev => setText(ev.target.value)} />
+						<div className="add" onClick={isUpdating ? () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating) : () => addToDo(text, setText, setToDo)}>
+							{isUpdating ? "Update" : "Add"}
+						</div>
 					</div>
-				</div>
 
-				<div className="list">
-					{toDo.map(item => (
-						<ToDoComponent key={item._id} text={item.text} updateMode={() => updateMode(item._id, item.text)} deleteToDo={() => deleteToDo(item._id, setToDo)} />
-					))}
-				</div>
+					<div>
+						{toDo.map(item => (
+							<ToDoComponent key={item._id} text={item.text} updateMode={() => updateMode(item._id, item.text)} deleteToDo={() => deleteToDo(item._id, setToDo)} />
+						))}
+					</div>
+				</Grid>
 			</Container>
 		</Box>
 	);
--- frontend/src/components/ToDoComponent.jsx
+++ frontend/src/components/ToDoComponent.jsx
@@ -1,16 +1,19 @@
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Box, Grid } from "@mui/material";
 
 const ToDoComponent = ({ text, updateMode, deleteToDo }) => {
 	return (
-		<div className="toDo">
-			<div className="text">{text}</div>
-			<div className="icons">
-				<FontAwesomeIcon className="icon" icon={faPen} onClick={updateMode} />
-				<FontAwesomeIcon className="icon" icon={faTrash} onClick={deleteToDo} />
-			</div>
-		</div>
+		<Box className="toDo">
+			<Grid container>
+				<div className="text">{text}</div>
+				<div className="icons">
+					<FontAwesomeIcon className="icon" icon={faPen} onClick={updateMode} />
+					<FontAwesomeIcon className="icon" icon={faTrash} onClick={deleteToDo} />
+				</div>
+			</Grid>
+		</Box>
 	);
 };
 
